32992f6d99230275b157296ad8ac6019f1a402e0
Anthony Nowell <anthony@algorithmia.com>
2015-06-19 18:37:12 -0600

    Cache APIAddress internally

--- src/main/java/com/algorithmia/AlgorithmiaConf.java
+++ src/main/java/com/algorithmia/AlgorithmiaConf.java
@@ -7,23 +7,29 @@ package com.algorithmia;
 public class AlgorithmiaConf {
     private AlgorithmiaConf() {}
 
+    private static String apiAddress;
+
     /**
      * Returns the base address of the API server (https://api.algorithmia.com)
      * @return the base address of the API server
      */
     public static String apiAddress() {
-        // System environment variable
-        final String envApiAddress = System.getenv("ALGORITHMIA_API");
-        // Java property variable
-        final String propertyApiAddress = System.getProperty("ALGORITHMIA_API");
-        if(propertyApiAddress != null && propertyApiAddress.trim().length() > 0) {
-            return propertyApiAddress;
-        } else if(envApiAddress != null && envApiAddress.trim().length() > 0) {
-            return envApiAddress;
-        } else {
-            // Default to official Algorithmia API
-            return "https://api.algorithmia.com";
+        // Cache the API Address
+        if(apiAddress == null) {
+            // System environment variable
+            final String envApiAddress = System.getenv("ALGORITHMIA_API");
+            // Java property variable
+            final String propertyApiAddress = System.getProperty("ALGORITHMIA_API");
+            if(propertyApiAddress != null && propertyApiAddress.trim().length() > 0) {
+                apiAddress = propertyApiAddress;
+            } else if(envApiAddress != null && envApiAddress.trim().length() > 0) {
+                apiAddress = envApiAddress;
+            } else {
+                // Default to official Algorithmia API
+                apiAddress = "https://api.algorithmia.com";
+            }
         }
+        return apiAddress;
     }
 
 }

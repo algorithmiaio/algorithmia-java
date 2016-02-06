660329046984d99d3175ded190c00fa89dd753e1
zeryx <james.sutton1211@gmail.com>
2016-02-06 16:09:53 -0400

    Added abstract base class for pipe() checking of class object inputs. Created test to check if class input is parsing properly.

---
+++ src/main/java/com/algorithmia/algo/AlgoInheritable.java
@@ -0,0 +1,9 @@
+package com.algorithmia.algo;
+
+/**
+ * Created by james on 06/02/16.
+ */
+
+//basic class used for checking with pipe, inherit this class before passing a class object to .pipe, as it will parse properly then.
+
+public abstract class AlgoInheritable {}

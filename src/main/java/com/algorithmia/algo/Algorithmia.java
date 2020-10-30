3e4ed3a4f945fa49fc27038c728c79f6704bc177
Dale Herring <dale.herring@ruralsourcing.com>
2020-10-29 19:27:10 -0500

    DEV-57 clean up import

--- src/main/java/com/algorithmia/algo/Algorithmia.java
+++ src/main/java/com/algorithmia/algo/Algorithmia.java
@@ -4,8 +4,6 @@ import com.algorithmia.client.SimpleAuth;
 import com.algorithmia.data.DataDirectory;
 import com.algorithmia.data.DataFile;
 
-import java.io.IOException;
-
 /**
  * Instantiate Algorithmia clients for calling algorithms and accessing data
  */
